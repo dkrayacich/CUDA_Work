f897e97648658ff562317d0009b0c271166c3e31
dkrayacich <163041227+dkrayacich@users.noreply.github.com>
2026-03-08 14:08:49 -0400

    Filled in rest of params

--- Cuda_MP1.c
+++ Cuda_MP1.c
@@ -14,22 +14,22 @@ int main(int argc, char* argv[]) {
 		cudaDeviceProp dp;
 		cudaGetDeviceProperties(&dp, d);
 
-		printf("Device %d --> Clock Rate: %d \n"
+		printf("Device %s --> Clock Rate: %d \n"
 			"Num SMs: %d \n"
-			"Num Cores: %d \n"
+			"Num Cores: 4864 \n" //this number was googled based on the type of GPU as it was not included in the deviceProp Struct
 			"Warp Size: %d \n"
-			"Global Mem: %zu \n"
-			"Constant Mem: %zu \n"
-			"Shared Mem Per Block: %zu \n"
+			"Global Mem (bytes): %zu \n"
+			"Constant Mem (bytes): %zu \n"
+			"Shared Mem Per Block (bytes): %zu \n"
 			"Num Regs Available Per Block: %d \n"
 			"Max Threads Per Block: %d \n"
-			"Block X Dim: %d \n"
-			"Block Y DIm: %d \n"
-			"Block Z Dim: %d \n"
-			"Grid X Dim: %d \n"
-			"Grid Y Dim: %d \n"
-			"Grid Z Dim: %d \n",
-			d, dp.clockRate, dp.multiProcessorCount, d, dp.warpSize, dp.totalGlobalMem,
+			"Max Block X Dim: %d \n"
+			"Max Block Y DIm: %d \n"
+			"Max Block Z Dim: %d \n"
+			"Max Grid X Dim: %d \n"
+			"Max Grid Y Dim: %d \n"
+			"Max Grid Z Dim: %d \n",
+			dp.name, dp.clockRate, dp.multiProcessorCount, dp.warpSize, dp.totalGlobalMem,
 			dp.totalConstMem, dp.sharedMemPerBlock, dp.regsPerBlock, dp.maxThreadsPerBlock,
 			dp.maxThreadsDim[0], dp.maxThreadsDim[1], dp.maxThreadsDim[2], dp.maxGridSize[0],
 			dp.maxGridSize[1], dp.maxGridSize[2]);
