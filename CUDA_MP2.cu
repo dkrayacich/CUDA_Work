63c1058bf4bd178058d1f5c32243cd23edc167e4
dkrayacich <163041227+dkrayacich@users.noreply.github.com>
2026-03-08 13:00:09 -0400

    Finished timing - just part B left

--- CUDA_MP2.cu
+++ CUDA_MP2.cu
@@ -4,7 +4,7 @@
 #include <device_launch_parameters.h>
 #include <stdlib.h>
 #include <stdio.h>
-#define BLOCK_WIDTH 16
+#define BLOCK_WIDTH 15
 
 __global__ void matMulKernel(float* M, float* N, float* P, int Width) {
 
@@ -22,6 +22,19 @@ __global__ void matMulKernel(float* M, float* N, float* P, int Width) {
 
 }
 
+__global__ void matMulKernelB(float* M, float* N, float* P, int Width) {
+
+	for (int i = 0; i < Width; i++) {
+		for (int j = 0; j < Width; j++) {
+			float Pvalue = 0;
+			for (int k = 0; k < Width; k++) {
+				Pvalue += M[i * Width + k] * N[k * Width + j];
+			}
+			P[i * Width + j] = Pvalue;
+		}
+	}
+}
+
 
 void matMul(float* M, float* N, float* P, int Width) {
 	int size = Width * Width * sizeof(float);
@@ -29,24 +42,12 @@ void matMul(float* M, float* N, float* P, int Width) {
 	float* d_N;
 	float* d_P;
 
-	cudaEvent_t start, stop;
-	cudaEventCreate(&start);
-	cudaEventCreate(&stop);
-	cudaDeviceSynchronize();
-
 	cudaMalloc((void**)&d_M, size);
 	cudaMalloc((void**)&d_N, size);
 	cudaMalloc((void**)&d_P, size);
 
-	cudaEventRecord(start, 0);
 	cudaMemcpy(d_M, M, size, cudaMemcpyHostToDevice);
 	cudaMemcpy(d_N, N, size, cudaMemcpyHostToDevice);
-	cudaEventRecord(stop, 0);
-	cudaEventSynchronize(stop);
-
-	float time;
-	cudaEventElapsedTime(&time, start, stop);
-	printf("Host to Dev transfer time (ms): %f \n", time);
 
 	int NumBlocks = Width / BLOCK_WIDTH;
 	if (Width % BLOCK_WIDTH) NumBlocks++;
@@ -54,17 +55,30 @@ void matMul(float* M, float* N, float* P, int Width) {
 	dim3 dimGrid(NumBlocks, NumBlocks);
 	dim3 dimBlock(BLOCK_WIDTH, BLOCK_WIDTH);
 
-	matMulKernel << <dimGrid, dimBlock >> > (d_M, d_N, d_P, Width);
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+	cudaDeviceSynchronize();
+
+	cudaEventRecord(start, 0);
+	//matMulKernel << <dimGrid, dimBlock >> > (d_M, d_N, d_P, Width); //for regular matrix mult
+	matMulKernelB << <1, 1 >> > (d_M, d_N, d_P, Width); //for case of 1 block with with one thread
+	cudaEventRecord(stop, 0);
+	cudaEventSynchronize(stop);
+
+	float time;
+	cudaEventElapsedTime(&time, start, stop);
+	printf("Mat Mult Kernel Time (ms): %f \n", time);
 
 	cudaMemcpy(P, d_P, size, cudaMemcpyDeviceToHost);
-
+	
 	cudaFree(d_M);
 	cudaFree(d_N);
 	cudaFree(d_P);
 }
 
 int main(int argc, char* argv[]) {
-	int Width = 1500;
+	int Width = 300;
 	int size = Width * Width * sizeof(float);
 	float* M = (float*)malloc(size * sizeof(float));
 	float* N = (float*)malloc(size * sizeof(float));
@@ -75,11 +89,6 @@ int main(int argc, char* argv[]) {
 		M[i] = ((float)rand() / RAND_MAX) * 100; //make random floating point numbers between 0 and 100
 		N[i] = ((float)rand() / RAND_MAX) * 100;
 	}
-
-	cudaEvent_t start, stop;
-	cudaEventCreate(&start);
-	cudaEventCreate(&stop);
-	cudaDeviceSynchronize();
 
 	matMul(M, N, P_g, Width);
 
@@ -103,7 +112,3 @@ int main(int argc, char* argv[]) {
 		printf("Test PASSED");
 	}
 }
-
-
-
-
